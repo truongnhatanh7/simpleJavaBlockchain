5380dd096f26c55bc2eb41c3c0929d0bdb8457b6
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-12 10:27:33 +0700

    Initial commit

---
+++ src/com/ftc/Block.java
@@ -0,0 +1,55 @@
+package com.ftc;
+
+public class Block {
+    private String hash;
+    private String prevHash;
+    private String data;
+    private long timestamp;
+    private int nonce;
+
+    public Block(String prevHash, String data, long timestamp) {
+        this.prevHash = prevHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getPrevHash() {
+        return prevHash;
+    }
+
+    public void setPrevHash(String prevHash) {
+        this.prevHash = prevHash;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(int nonce) {
+        this.nonce = nonce;
+    }
+}
